be52e0d7490977b0e80404acd123c1a3bf2632ba
Vladislav Maslikhov <55663958+LiveOutside@users.noreply.github.com>
2021-12-29 00:06:02 +0500

    Update tiles.py

--- tiles.py
+++ tiles.py
@@ -5,7 +5,7 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
         self.image = pygame.Surface((size, size))
-        self.image.fill(pygame.Color('grey'))
+        self.image.fill((0, 135, 75))
         self.rect = self.image.get_rect(topleft=pos)
 
     def update(self, x_shift):
