49133861f357faf83286f5bf2e30062a3ddb398e
MyuzZ <94909786+MyuzZ@users.noreply.github.com>
2022-01-22 16:24:32 +0500

    Update music.py

--- music.py
+++ music.py
@@ -3,16 +3,9 @@ import pygame
 pygame.init()
 
 
-def musicplayer():
-    pygame.mixer.music.load("")  # вставить название музыки, музыка должна быть в одной папке с основным кодом
+def musicplayer(musicname):
+    pygame.mixer.music.load(musicname)
     pygame.mixer.music.play()
-    #
-    W, H = 500, 300                       # стереть т.к. функция встанет
-    sc = pygame.display.set_mode((W, H))  # в готовый дисплей
-
-    clock = pygame.time.Clock  # вставить параметры из main
-    FPS = 60                   #
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
